d14cc6566f46433a320a78083de524a25aec528f
Xiang Li <lostxine@gmail.com>
2023-01-03 20:35:21 -0500

    test code

---
+++ fallguysenv.py
@@ -0,0 +1,39 @@
+import socket
+import numpy as np
+import cv2 
+import time
+
+
+class FallGuysEnv:
+    def __init__(self, udp_target=('127.0.0.1', 5005), camera_idx=0) -> None:
+        self.target = udp_target
+        self.socket = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+        self.sock.settimeout(0.5)
+
+        self.cam = cv2.VideoCapture(camera_idx)
+
+
+    def reset(self):
+        ret, frame = self.cam.read()
+        while True:
+            try:
+                cv2.imshow('Frame', frame)
+                cv2.waitKey(5)
+            except KeyboardInterrupt:
+                break
+
+    def __del__(self):
+        cv2.destroyAllWindows()
+        if self.cam is not None:
+            self.cam.release()
+
+    def step(self, action):
+        self.socket.sendto(action.astype(np.float32).tobytes(), self.target)
+        
+
+
+
+if __name__ == '__main__':
+    env = FallGuysEnv()
+    env.reset()
